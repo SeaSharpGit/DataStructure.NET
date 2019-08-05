d4e97ebd0530b2c03c61386362176bac68d3b944
SEA-PC <18912387311@163.com>
2019-08-05 17:26:38 +0800

    单链队列

---
+++ DataStructure.NET/线性表/队列/单链队列/LinkQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.NET
+{
+    public class LinkQueue<T> : IMyQueue<T>
+    {
+        private Node<T> _Front = null;
+        private Node<T> _Rear = null;
+        private int _Size = 0;
+
+        public int GetLength()
+        {
+            return _Size;
+        }
+
+        public bool IsEmpty() => _Size == 0;
+
+        public void Clear()
+        {
+            _Size = 0;
+            _Front = null;
+            _Rear = null;
+        }
+
+        public void In(T item)
+        {
+            if (IsEmpty())
+            {
+
+            }
+        }
+
+        public T Out()
+        {
+
+        }
+
+        public T GetFront()
+        {
+
+        }
+
+
+    }
+}
